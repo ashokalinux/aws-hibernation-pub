66f73263b842c6aa78d8223463858f32f7217842
Ashok Shelke <ashok.shelke@modusbox.com>
2021-07-22 17:44:26 +0530

    add scripts

---
+++ EC2-start/lambda_function.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# Purpose: Start all the instances having tag "Enviornment=dev" AND "Tenant=prod-dev"
+# Ver 1.0
+# Tested for Python 2.7, 3.8
+# Author: Ashok Shelke
+# -----------------------------------------------------------------------------------
+import boto3
+import os
+# Enter the region your instances are in under "Environment variables" section of "Configuration" tab of lambda, e.g. 'key=REGION'; 'value=us-east-1'
+
+#Variables
+region= os.environ["REGION"]
+
+#define the connection and set the region
+ec2 = boto3.client('ec2', region)
+def lambda_handler(event, context):
+    instancelst=[]
+    reservations = ec2.describe_instances(
+        Filters=[
+            {'Name': 'tag:Environment', 'Values': ['dev']},
+            {'Name': 'tag:Tenant', 'Values': ['prod-dev']},
+            {'Name': 'instance-state-name', 'Values': ['stopped']}
+
+        ]
+    ).get(
+        'Reservations', []
+    )
+
+    instances = sum(
+        [
+            [i for i in r['Instances']]
+            for r in reservations
+        ], [])
+
+    for instance in instances:
+        instancelst.append(instance['InstanceId'])
+
+#print StartedInstances  
+    if len(instancelst) > 0:
+        ec2.start_instances(InstanceIds=instancelst)
+        print('started your instances: ' + str(instancelst))
+    else:
+        print("No instance with given tag value is in stopped state")
---
+++ EC2-stop/lambda_function.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# Purpose: Start all the instances having tag "Enviornment=dev" AND "Tenant=prod-dev"
+# Ver 1.0
+# Tested for Python 2.7, 3.8
+# Author: Ashok Shelke
+# -----------------------------------------------------------------------------------
+import boto3
+import os
+
+# Enter the region your instances are in, e.g. 'us-east-1'
+
+#Variables
+region= os.environ["REGION"]
+
+ec2 = boto3.client('ec2', region_name=region)
+def lambda_handler(event, context):
+    instancelst=[]
+    reservations = ec2.describe_instances(
+        Filters=[
+            {'Name': 'tag:Environment', 'Values': ['dev']},            
+            {'Name': 'tag:Tenant', 'Values': ['prod-dev']},
+            {'Name': 'instance-state-name', 'Values': ['running']}
+        ]
+    ).get(
+        'Reservations', []
+    )
+
+    instances = sum(
+        [
+            [i for i in r['Instances']]
+            for r in reservations
+        ], [])
+
+    for instance in instances:
+        instancelst.append(instance['InstanceId'])
+    
+#    print('stopped your instances: ' + str(instancelst))
+#print StoppedInstances  
+    if len(instancelst) > 0:
+        ec2.stop_instances(InstanceIds=instancelst)
+        print('stopped your instances: ' + str(instancelst))
+    else:
+        print("No instance with given tag value is in running state")
